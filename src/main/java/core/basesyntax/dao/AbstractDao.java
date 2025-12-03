f778416b9a5b763376d2a23bf887eeecf065e5f9
Kamil Jan Mularski <kammul@outlook.com>
2025-12-03 22:15:03 +0100

    Update AbstractDao.java
    The saveEntity() method was created.

--- src/main/java/core/basesyntax/dao/AbstractDao.java
+++ src/main/java/core/basesyntax/dao/AbstractDao.java
@@ -1,11 +1,35 @@
 package core.basesyntax.dao;
 
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public abstract class AbstractDao {
     protected final SessionFactory sessionFactory;
 
     protected AbstractDao(SessionFactory sessionFactory) {
         this.sessionFactory = sessionFactory;
+    }
+
+    protected <T> T saveEntity(T entity) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.save(entity);
+            transaction.commit();
+        } catch (Exception ex) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new HibernateException("Cannot save: " + entity, ex);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return entity;
     }
 }
