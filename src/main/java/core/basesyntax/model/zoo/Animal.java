3ca5ae108f2da6bf1787275925d6b558522b8e19
Kamil Jan Mularski <kammul@outlook.com>
2025-12-03 22:58:24 +0100

    Update Animal.java and Machine.java

--- src/main/java/core/basesyntax/model/zoo/Animal.java
+++ src/main/java/core/basesyntax/model/zoo/Animal.java
@@ -1,6 +1,5 @@
 package core.basesyntax.model.zoo;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
